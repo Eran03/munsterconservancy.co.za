79de85c36651763f187cdb322f4b72cf31b30e4d
Eran <eranbroide@gmail.com>
2023-10-25 23:51:59 +0200

    Added page and script to test API live

---
+++ testAPI.js
@@ -0,0 +1,52 @@
+const accessToken = 'i0w6bucsgxu3jc7nchdjoash1htquwx1slocid7r';
+const numBlogsPerPage = 2;
+let page = 1;
+
+function fetchBlogs(page) {
+    const url = 'https://api.medium.com/v1/users/wildlifeconservationsociety/posts?limit=${numBlogsPerPage}&page=${page}';
+    const headers = {
+        'Authorization': 'Bearer ${accessToken}'
+};
+
+fetch(url, { headers })
+    .then(response => response.json())
+    .then(data => {
+        const blogsDiv = document.getElementById('blogs');
+        const paginationDiv = document.getElementById('pagination');
+
+        data.data.forEach(blog => {
+            document.getElementById("blogs").innerHTML = document.getElementById("blogs").innerHTML + "<div id='item-" + key + "' </div>";
+
+            document.getElementById("item-" + key).innerHTML =
+                "<h2 class='title'>" + item.title + "</h2>" +
+                "<h5 class='pub-date'>" + item.pubDate + "</h5>" +
+                "<br>" +
+                "<p class='content'>" + item.content + "</p>" +
+                "<p class='link'><a href='" + item.link + "' target='_blank'>" + item.link + "</a></p>" +
+                "<br><hr><br>";
+        });
+
+        // Create pagination buttons (Next, Previous) and attach event listeners
+    })
+    .catch(error => console.error('Error fetching blogs:', error));
+}
+
+fetchBlogs(page);
+
+function showNextPage() {
+    page++;
+    fetchBlogs(page);
+}
+
+function showPreviousPage() {
+    if (page > 1) {
+        page--;
+        fetchBlogs(page);
+    }
+}
+
+const paginationDiv = document.getElementById('pagination');
+paginationDiv.innerHTML = `
+    <button onclick="showPreviousPage()">Previous</button>
+    <button onclick="showNextPage()">Next</button>
+`;
